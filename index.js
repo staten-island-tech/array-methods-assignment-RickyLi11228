a6cf41354013de7215cd3425fbfe785816ff9a4c
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-13 09:28:13 -0400

    donezzzzzzzzzzz
    ezzzzzzzzzzz

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const students = [{
     firstname: "Hairy", 
     lastname: "Li", 
     intelligent: false, 
-    graduating: true, 
+    friends: true, 
     siblings: ["Brain", "Brairy"] 
 },{
     firstname: "Ty", 
     lastname: "Yippee", 
     intelligent: false, 
-    graduating: false, 
+    friends: false, 
     siblings: ["Tap","Yap"]
 },{
     firstname: "Devin", 
     lastname: "Chen", 
     intelligent: false, 
-    graduating: false,
+    friends: false,
     siblings: false,
 }]
-students.forEach((student)=> console.log(student.siblings))
+
+const friendless = students.filter((students) => students.friends === true)
+console.log(friendless)
+const has_siblings = students.filter((students) => students.siblings !== false)
+console.log(has_siblings)
+const sibling = students.forEach((students)=> students.siblings.forEach((siblings) => console.log(siblings)));
 students.forEach((student)=> console.log(student.firstname))
-const graduating = students.filter((student) => student.graduating === true)
-console.log(graduating)
-const siblings = students.filter((student) => student.siblings !== false)
-console.log(siblings)
