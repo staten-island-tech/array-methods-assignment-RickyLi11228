d8c6b212e7f7cacf07e9cb89340f1e316767eacb
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-16 09:56:54 -0400

    doneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ const friendless = students.filter((students) => students.friends === true)
 console.log(friendless)
 const has_siblings = students.filter((students) => students.siblings !== false)
 console.log(has_siblings)
-const sibling = students.forEach((students)=> students.siblings.forEach((siblings) => console.log(siblings)));
+const sibling = students.forEach((students)=> students.siblings.forEach((siblings) => console.log(siblings)))
 students.forEach((student)=> console.log(student.firstname))
